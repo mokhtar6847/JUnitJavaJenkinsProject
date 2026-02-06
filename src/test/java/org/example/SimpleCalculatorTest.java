d6a3db517c9d828bed37375d1d9ce6626b6e633e
Mokhtar METEHRI <mokhtarmetehri@yahoo.fr>
2026-02-07 00:28:59 +0100

    new commit 07022026

--- src/test/java/org/example/SimpleCalculatorTest.java
+++ src/test/java/org/example/SimpleCalculatorTest.java
@@ -8,7 +8,7 @@ class SimpleCalculatorTest {
     @Test
             void twoPlusThreeEqualsFive() {
         SimpleCalculator calculation = new SimpleCalculator();
-        assertEquals(12, calculation.add(6, 6));
+        assertEquals(12, calculation.add(6, 6g));
 
     }
 
